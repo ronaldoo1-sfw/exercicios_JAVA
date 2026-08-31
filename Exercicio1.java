080c7d740517a3090470143c934aca5bd243c2d9
Ronaldo Bordin Pugina <ronaldobordin2016@gmail.com>
2026-08-31 11:45:49 -0300

    Exercicio1 em java

---
+++ Exercicio1.java
@@ -0,0 +1,39 @@
+//a.1)Acrescente mais uma linha de exibição no programa anterior.
+/*
+public class Exercicio1 {
+
+	public static void main(String arg[]){
+	
+		System.out.println(“Facim, facim!!!");
+		System.out.println(“Facim, facim!!!");
+	}
+}
+*/
+
+//a.2)Crie um programa que imprima todos os seus dados cadastrais.
+/*
+public class Exercicio1 {
+
+	public static void main(String arg[]){
+		
+		System.out.println("Meu nome é: Ronaldo");
+		System.out.println("Tenho 21 anos");
+		System.out.println("Meu email e ronaldobordin2016@gmail.com");
+		System.out.println("Meu numero e 18991468636");
+	
+	}
+	
+}
+*/
+
+//a.3)Crie um programa que imprima a frase “Meu nome é: ” que através de parâmetros passados durante a chamada (interpretação: java ...) doprograma, imprima na tela esta frase + o nome passado.
+
+public class Exercicio1 {
+
+	public static void main(String arg[]){
+		
+		System.out.println("Meu nome é: "+ arg[0]);
+	
+	}
+	
+}
