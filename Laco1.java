b3f5dfc3b06d6099a1d980d609d056951c952b0a
Ronaldo Bordin Pugina <ronaldobordin2016@gmail.com>
2026-08-31 12:13:05 -0300

    Exercicio com laço for

---
+++ Laco1.java
@@ -0,0 +1,16 @@
+/*c.1) Usando o laço “for”
+, construa umprograma que ao invés de incrementar a variável “a” até que atinja o valor dedavariável “b”
+, faça o inverso, decremente “b” até que achegueaovalorde “a”.*/
+
+public class Laco1 {
+
+	public static void main(String arg[]){
+	
+	int a=0;
+	int b=10;
+	
+	for(b=10; b>=a; b--){
+	System.out.println("O Valor de B dentro do FOR é: "+b);}
+	
+	}
+}
