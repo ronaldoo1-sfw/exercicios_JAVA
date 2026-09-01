81734a8cbc11f5fa46e41c1e16f902810bb1f319
Ronaldo Bordin Pugina <ronaldobordin2016@gmail.com>
2026-09-01 17:59:03 -0300

    Laco 2 em java

---
+++ Laco2.java
@@ -0,0 +1,13 @@
+//d.1)_ Da mesma forma que implementou o exercícioc.1, implemente o d.1 usando a estrutura de repetição“while”
+
+public class Laco2 {
+	
+	public static void main(String arg[]){
+	int a=0, b=15;
+	
+	while(b>=a){
+	b--;
+	System.out.println("O Valor de B dentro do while é: "+b);}
+	
+	}
+}
