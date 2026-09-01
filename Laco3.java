989fbe33c5a09e88bb84704bf194b979eee8e020
Ronaldo Bordin Pugina <ronaldobordin2016@gmail.com>
2026-09-01 18:00:50 -0300

    Laco 3 em java

---
+++ Laco3.java
@@ -0,0 +1,16 @@
+/*e)Da mesma forma que implementou o exercícioc.1,
+implemente o e.1 usando a estrutura de repetição“do..while”*/
+
+public class Laco3 {
+	public static void main(String arg[]){
+	
+	int a=0, b=15;
+	
+	do{
+		System.out.println("O valor de B dentro do do laço3 é: "+b);b--;
+	}while(b>=a);
+	
+	System.out.println("");
+	
+	}
+}
